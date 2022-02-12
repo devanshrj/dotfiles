665220615e9eb106f205a7c00f3959d3cfc694e9
devanshrj <devanshrj@outlook.com>
2022-02-13 01:58:17 +0530

    updated installation script

--- install.sh
+++ install.sh
@@ -15,34 +15,36 @@ if test ! $(which brew); then
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
+# variables
+dir=~/.dotfiles                    # dotfiles directory
+olddir=~/.dotfiles_old             # old dotfiles backup directory
+files="alacritty.yml gitconfig tmux.conf vimrc zshrc"    # list of files to symlink
+# folders="oh-my-zsh vim"            # list of folders to symlink
+
 # update Homebrew recipes
 brew update
 
 # install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
-brew bundle --file $DOTFILES/Brewfile
+brew bundle --file $dir/Brewfile
 
-# create symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# variables
-dir=~/.dotfiles                    # dotfiles directory
-olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="alacritty.yml gitconfig tmux.conf vimrc zshrc"    # list of files to symlink
-folders="oh-my-zsh vim"            # list of folders to symlink
+# install zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
+echo
+echo "------------"
+echo "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir $olddir
 echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
+    echo
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/$olddir/
+    mv ~/.$file $olddir/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/.$file ~/.$file
 done
